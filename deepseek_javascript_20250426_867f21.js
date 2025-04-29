80b7c23e2936bf4bd1baa0a4b1d8627bdf292d67
ahmedhany30 <67554897+ahmedhany30@users.noreply.github.com>
2025-04-29 21:55:58 +0300

    Add files via upload

--- deepseek_javascript_20250426_867f21.js
+++ deepseek_javascript_20250426_867f21.js
@@ -569,19 +569,3 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = 'auto';
     });
 });
-
-
-// في ملف service-worker.js
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/app.js'
-        ]);
-      })
-    );
-  });
-  
